1fc71d8c4c25eba32f98f75bb22a3e5174418473
Solomon Wiznitzer <shlomo.wiz@gmail.com>
2020-12-23 19:10:34 -0600

    Adjusts install scripts to account for some changes in the toolbox and core repos

--- interbotix_ros_xslocobots/install/amd64/xslocobot_amd64_install.sh
+++ interbotix_ros_xslocobots/install/amd64/xslocobot_amd64_install.sh
@@ -6,6 +6,8 @@ if [ $ubuntu_version == "16.04" ]; then
   ROS_NAME="kinetic"
 elif [ $ubuntu_version == "18.04" ]; then
   ROS_NAME="melodic"
+# elif [ $ubuntu_version == "20.04" ]; then
+#   ROS_NAME="noetic"
 else
   echo -e "Unsupported Ubuntu verison: $ubuntu_version"
   echo -e "Interbotix Locobot only works with 16.04 or 18.04"
@@ -13,10 +15,6 @@ else
 fi
 
 echo "Ubuntu $ubuntu_version detected. ROS-$ROS_NAME chosen for installation.";
-if [ $ROS_NAME == "melodic" ]; then
-  echo "There are no patches available for Kernel 5.4 at this time - needed for the RealSense Camera to work properly."
-  echo "The camera should still work with ROS but warnings will appear in your terminal window..."
-fi
 
 echo -e "\e[1;33m ******************************************** \e[0m"
 echo -e "\e[1;33m The installation may take around 15 Minutes! \e[0m"
@@ -30,8 +28,13 @@ sudo apt autoremove
 
 # Install some necessary core packages
 sudo apt -y install openssh-server
-sudo apt -y install python-pip
-sudo -H pip install modern_robotics
+if [ $ROS_NAME != "noetic" ]; then
+  sudo apt -y install python-pip
+  sudo -H pip install modern_robotics
+else
+  sudo apt -y install python3-pip
+  sudo -H pip3 install modern_robotics
+fi
 
 # Step 1: Install ROS
 if [ $(dpkg-query -W -f='${Status}' ros-$ROS_NAME-desktop-full 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
@@ -44,7 +47,11 @@ if [ $(dpkg-query -W -f='${Status}' ros-$ROS_NAME-desktop-full 2>/dev/null | gre
     sudo rm /etc/ros/rosdep/sources.list.d/20-default.list
   fi
   echo "source /opt/ros/$ROS_NAME/setup.bash" >> ~/.bashrc
-  sudo apt -y install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+  if [ $ROS_NAME != "noetic" ]; then
+    sudo apt -y install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+  else
+    sudo apt -y install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+  fi
   sudo rosdep init
   rosdep update
 else
@@ -60,19 +67,22 @@ if [ $(dpkg-query -W -f='${Status}' librealsense2 2>/dev/null | grep -c "ok inst
   sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
   sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo $(lsb_release -sc) main" -u
   if [ $ubuntu_version == "16.04" ]; then
-    version="2.39.0-0~realsense0.3570"
+    version="2.40.0-0~realsense0.3813"
   elif [ $ubuntu_version == "18.04" ]; then
-    version="2.39.0-0~realsense0.3571"
+    version="2.40.0-0~realsense0.3814"
+  elif [ $ubuntu_version == "20.04" ]; then
+    version="2.40.0-0~realsense0.3815"
   fi
 
   sudo apt -y install librealsense2-udev-rules=${version}
-  # Patches for Kernel 5.4 is NOT supported yet, so don't even try to install the package below in Melodic
-  if [ $ROS_NAME == "kinetic" ]; then
-    sudo apt -y install librealsense2-dkms=1.3.12-0ubuntu1
-  fi
+  sudo apt -y install librealsense2-dkms
   sudo apt -y install librealsense2=${version}
   sudo apt -y install librealsense2-gl=${version}
+  sudo apt -y install librealsense2-gl-dev=${version}
+  sudo apt -y install librealsense2-gl-dbg=${version}
   sudo apt -y install librealsense2-net=${version}
+  sudo apt -y install librealsense2-net-dev=${version}
+  sudo apt -y install librealsense2-net-dbg=${version}
   sudo apt -y install librealsense2-utils=${version}
   sudo apt -y install librealsense2-dev=${version}
   sudo apt -y install librealsense2-dbg=${version}
@@ -90,7 +100,7 @@ if [ ! -d "$REALSENSE_WS/src" ]; then
   cd $REALSENSE_WS/src
   git clone https://github.com/IntelRealSense/realsense-ros.git
   cd realsense-ros/
-  git checkout 2.2.18
+  git checkout 2.2.20
   cd $REALSENSE_WS
   catkin_make clean
   catkin_make -DCATKIN_ENABLE_TESTING=False -DCMAKE_BUILD_TYPE=Release
@@ -128,17 +138,18 @@ if [ ! -d "$INTERBOTIX_WS/src" ]; then
     echo "Building Kobuki ROS packages from source..."
     git clone https://github.com/yujinrobot/kobuki
     cd kobuki
-    git checkout melodic
+    git checkout $ROS_NAME
     sudo rm -r kobuki_capabilities kobuki
     cd $INTERBOTIX_WS/src
   fi
   git clone https://github.com/Interbotix/interbotix_ros_core.git
   git clone https://github.com/Interbotix/interbotix_ros_rovers.git
-  cd interbotix_ros_rovers
-  git checkout $ROS_NAME
-  cd ..
   git clone https://github.com/Interbotix/interbotix_ros_toolboxes.git
-  cd $INTERBOTIX_WS/src/interbotix_ros_core/interbotix_ros_xseries/interbotix_xs_sdk
+  cd interbotix_ros_rovers && git checkout $ROS_NAME && cd ..
+  rm interbotix_ros_core/interbotix_ros_xseries/CATKIN_IGNORE
+  rm interbotix_ros_toolboxes/interbotix_xs_toolbox/CATKIN_IGNORE
+  rm interbotix_ros_toolboxes/interbotix_common_toolbox/interbotix_moveit_interface/CATKIN_IGNORE
+  cd interbotix_ros_core/interbotix_ros_xseries/interbotix_xs_sdk
   sudo cp 99-interbotix-udev.rules /etc/udev/rules.d/
   sudo udevadm control --reload-rules && sudo udevadm trigger
   cd $INTERBOTIX_WS
--- interbotix_ros_xslocobots/install/xslocobot_remote_install.sh
+++ interbotix_ros_xslocobots/install/xslocobot_remote_install.sh
@@ -27,8 +27,7 @@ if [ ! -d "$INTERBOTIX_WS/src" ]; then
   catkin_make
   cd src
   git clone https://github.com/Interbotix/interbotix_ros_rovers.git
-  # cd interbotix_ros_rovers
-  # git checkout $ROS_NAME
+  cd interbotix_ros_rovers && git checkout $ROS_NAME && cd ..
   echo "source $INTERBOTIX_WS/devel/setup.bash" >> ~/.bashrc
 else
   echo "Interbotix Locobot ROS packages already installed!"

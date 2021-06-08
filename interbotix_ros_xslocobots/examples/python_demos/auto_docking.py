634e2f450af48d1e52a865dc4193982717dcb4f5
Luke Schmitt <luke@trossenrobotics.com>
2021-06-08 13:34:16 -0500

    [xslocobots] auto_dock improvements and bugfixes
    Added a launch arg (dock) to xslocobots_control.launch
    and xslocobots_python.launch that, when true, launches the Kobuki
    AutoDockingNodelet.
    
    Improved the auto_docking example script, making it more descriptive.
    Addded a more accurate and general roslaunch tip.

--- interbotix_ros_xslocobots/examples/python_demos/auto_docking.py
+++ interbotix_ros_xslocobots/examples/python_demos/auto_docking.py
@@ -4,12 +4,19 @@ from interbotix_xs_modules.locobot import InterbotixLocobotXS
 # This script sends the Locobot to its docking station to charge
 #
 # To get started, open a terminal and type...
-# 'roslaunch interbotix_xslocobot_control xslocobot_python.launch robot_model:=locobot_wx250s show_lidar:=true'
+# 'roslaunch interbotix_xslocobot_control xslocobot_python.launch robot_model:=locobot_MODEL use_base:=true dock:=true'
 # Then change to this directory and type 'python auto_docking.py'
 
+# Change this value to your locobot model
+MODEL = "locobot_px100"
+
 def main(): 
-    locobot = InterbotixLocobotXS(robot_model="locobot_wx250s")
-    locobot.base.auto_dock()
+    locobot = InterbotixLocobotXS(robot_model=MODEL)
+    if locobot.base.auto_dock():
+        print("Docking Successful.")
+    else:
+        print("Docking Unsuccessful.")
+
 
 if __name__ == "__main__":
     main()

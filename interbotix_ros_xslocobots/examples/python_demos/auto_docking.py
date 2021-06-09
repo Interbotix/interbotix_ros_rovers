547fb44af736778129cfa5ebb078d8bfeba1aa4d
Luke Schmitt <luke@trossenrobotics.com>
2021-06-09 14:33:44 -0500

    [xslocobots] publish auto_dock
    Modified launch files xslocobots_control.launch and
    xslocobots_python.launch to fix bug where odom topic was not remapping
    properly.
    
    Simplified auto_docking script before publishing. Added more descriptive
    instructions for users.

--- interbotix_ros_xslocobots/examples/python_demos/auto_docking.py
+++ interbotix_ros_xslocobots/examples/python_demos/auto_docking.py
@@ -2,20 +2,23 @@ import math
 from interbotix_xs_modules.locobot import InterbotixLocobotXS
 
 # This script sends the Locobot to its docking station to charge
-#
-# To get started, open a terminal and type...
-# 'roslaunch interbotix_xslocobot_control xslocobot_python.launch robot_model:=locobot_MODEL use_base:=true dock:=true'
-# Then change to this directory and type 'python auto_docking.py'
+#   For best performance, robot should be under 1m in front of the dock
+#   Note that this script should only be run on robots with smaller or no arms
+#       - locobot_base
+#       - locobot_px100
 
-# Change this value to your locobot model
+# Update this value with your model
 MODEL = "locobot_px100"
+
+# To get started, open a terminal on the robot and type...
+# 'roslaunch interbotix_xslocobot_control xslocobot_python.launch robot_model:=MODEL use_base:=true dock:=true'
+#                                                                                ^ update MODEL
+# Then open a new terminal in this directory and type 'python auto_docking.py'
+
 
 def main(): 
     locobot = InterbotixLocobotXS(robot_model=MODEL)
-    if locobot.base.auto_dock():
-        print("Docking Successful.")
-    else:
-        print("Docking Unsuccessful.")
+    locobot.base.auto_dock()
 
 
 if __name__ == "__main__":

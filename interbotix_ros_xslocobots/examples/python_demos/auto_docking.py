ba1479736ae39e1ff74ef7963cbe421f7b60d31f
Luke Schmitt <luke@trossenrobotics.com>
2021-07-12 14:15:27 -0500

    [xslocobots] Minor Changes
    Changed wording of dock enable param and simplified wording in autodock
    script.

--- interbotix_ros_xslocobots/examples/python_demos/auto_docking.py
+++ interbotix_ros_xslocobots/examples/python_demos/auto_docking.py
@@ -1,4 +1,4 @@
-from interbotix_xs_modules.locobot import InterbotixLocobotXS
+from interbotix_xs_modules.locobot import InterbotixLocobotXS as Locobot
 
 # This script sends the Locobot to its docking station to charge
 #   For best performance, robot should be under 1m in front of the dock
@@ -10,7 +10,7 @@ from interbotix_xs_modules.locobot import InterbotixLocobotXS
 MODEL = "px100"
 
 # To get started, open a terminal on the robot and type...
-# 'roslaunch interbotix_xslocobot_control xslocobot_python.launch robot_model:=MODEL use_base:=true dock:=true'
+# 'roslaunch interbotix_xslocobot_control xslocobot_python.launch robot_model:=locobot_MODEL use_base:=true use_dock:=true'
 #                                                                                ^ update MODEL
 # Then open a new terminal in this directory and type 'python auto_docking.py'
 
@@ -20,7 +20,7 @@ def main():
         print("This script should only be run on robots with smaller or no arms")
         exit()
 
-    locobot = InterbotixLocobotXS(robot_model="locobot_"+MODEL, use_dock=True)
+    locobot = Locobot(robot_model="locobot_"+MODEL, use_dock=True)
     locobot.base.auto_dock()
 
 

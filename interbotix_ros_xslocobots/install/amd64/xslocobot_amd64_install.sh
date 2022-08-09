e5416b0b0baf0778d28af29a47fab3e6e722825f
Luke Schmitt <luke@trossenrobotics.com>
2022-08-09 13:52:03 -0500

    [ros_xslocobots] Fix key issue in install script

--- interbotix_ros_xslocobots/install/amd64/xslocobot_amd64_install.sh
+++ interbotix_ros_xslocobots/install/amd64/xslocobot_amd64_install.sh
@@ -194,7 +194,6 @@ function install_ros1() {
   if [ $(dpkg-query -W -f='${Status}' ros-$ROS_DISTRO_TO_INSTALL-desktop-full 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     echo -e "${GRN}Installing ROS1 $ROS_DISTRO_TO_INSTALL...${OFF}"
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
     curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
     sudo apt update
     sudo apt -y install ros-$ROS_DISTRO_TO_INSTALL-desktop-full

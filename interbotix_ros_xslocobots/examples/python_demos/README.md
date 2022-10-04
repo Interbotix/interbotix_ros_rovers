dfe8698d100db8108d606201a9161eb037a4f6ff
Luke Schmitt <luke@trossenrobotics.com>
2022-10-03 20:10:52 -0500

    [xslocobot_control] Move Python demos to control
    Moves the Python-ROS2 API demos to the xslocobot_control package.

--- interbotix_ros_xslocobots/examples/python_demos/README.md
+++
@@ -1,9 +0,0 @@
-# Interbotix X-Series LoCoBot Python API Demos
-
-[![View Documentation](https://trossenrobotics.com/docs/docs_button.svg)](https://www.trossenrobotics.com/docs/interbotix_xslocobots/ros_packages/python_demos.html)
-
-## Overview
-
-This directory showcases various ways of using the [Interbotix Python LoCoBot Module](https://github.com/Interbotix/interbotix_ros_toolboxes/blob/main/interbotix_xs_toolbox/interbotix_xs_modules/src/interbotix_xs_modules/locobot.py) (click the link to see the fully documented code). Simply put, this API was created so that users with little to no ROS experience would still have the ability to control any Interbotix LoCoBot supported by the *interbotix_xs_sdk*. Specifically, the API also allows a user to make an arm go to desired end-effector poses or follow Cartesian trajectories. This last feature was made possible by the [Modern Robotics: Mechanics, Planning, and Control Code Library](https://github.com/NxRLab/ModernRobotics) created at Northwestern University. It also allows the ability to move the camera pan/tilt servos and send velocity commands to the base.
-
-For the API to work, the arm and pan/tilt joints must be set to 'position' control and the gripper set to 'PWM' control (conveniently, these are the default configs in the *interbotix_xslocobot_control* package). Furthermore, the API assumes that all the arm-joint motors' [Drive Mode](http://emanual.robotis.com/docs/en/dxl/x/xm430-w350/#drive-mode) registers are set to [Time-Based-Profile](http://emanual.robotis.com/docs/en/dxl/x/xm430-w350/#profile-velocity112) (this is also the default configuration). In a nutshell, this setting makes it very easy for you as the user to customize the duration and smoothness of an arm's motion from one pose to the next.
---
+++ interbotix_ros_xslocobots/interbotix_xslocobot_control/demos/python_ros2_api/bartender.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 Trossen Robotics
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import math
+
+from interbotix_xs_modules.xs_robot.locobot import InterbotixLocobotXS
+
+"""
+This script makes the end-effector perform pick, pour, and place tasks.
+
+To get started, open a terminal and run the command:
+
+    ros2 launch interbotix_xslocobot_control xslocobot_control.launch robot_model:=locobot_wx250s
+
+Then change to this directory and run the command:
+
+    python3 bartender.py
+
+...or run the command:
+
+    ros2 run interbotix_xslocobot_control bartender.py
+"""
+
+
+def main():
+    locobot = InterbotixLocobotXS(
+        robot_model='locobot_wx250s',
+        arm_model='mobile_wx250s'
+    )
+
+    locobot.arm.set_ee_pose_components(x=0.3, z=0.2)
+    locobot.arm.set_single_joint_position('waist', math.pi/4.0)
+    locobot.gripper.release()
+    locobot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.25)
+    locobot.gripper.grasp()
+    locobot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.25)
+    locobot.arm.set_single_joint_position('waist', -math.pi/4.0)
+    locobot.arm.set_ee_cartesian_trajectory(pitch=1.5)
+    locobot.arm.set_ee_cartesian_trajectory(pitch=-1.5)
+    locobot.arm.set_single_joint_position('waist', math.pi/4.0)
+    locobot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.25)
+    locobot.gripper.release()
+    locobot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.25)
+    locobot.arm.go_to_home_pose()
+    locobot.arm.go_to_sleep_pose()
+
+    locobot.shutdown()
+
+
+if __name__ == '__main__':
+    main()
